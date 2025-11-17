af7c3eb69a4bb4af6d204813eb471b52c7b0fe59
Tolotra Rakotomalala Andriamahefa <toloandr@c1r4s8.42antananarivo.mg>
2025-11-17 11:32:47 +0300

    from collections import OrderedDict

--- test.py
+++ test.py
@@ -18,11 +18,26 @@
 # for element in my_counter.elements():
 #     print(element)
 
-################################################
-def p():
-	print("Hey")
+# ################################################
+# def p():
+# 	print("Hey")
 
-from collections import namedtuple
-Point = namedtuple('Point', 'x,y,z')
-pt = Point(1, p, 5)
-print(pt.y())
+# from collections import namedtuple
+# Point = namedtuple('Point', 'x,y,z')
+# pt = Point(1, p, 5)
+# print(pt.y())
+
+
+# ################################################
+from collections import OrderedDict
+
+my_dict = OrderedDict()
+my_dict['a'] = 1
+my_dict['b'] = 2 
+my_dict['c'] = 3 
+my_dict['d'] = 4 
+my_dict['e'] = 5 
+
+print(my_dict)
+for (k, v) in my_dict.items():
+	print(f"k={k} v={v}")
