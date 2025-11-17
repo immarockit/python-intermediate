8e0f1df3bbe3232d7ce3c499c6a63cb5b0c3f529
Tolotra Rakotomalala Andriamahefa <toloandr@c1r4s8.42antananarivo.mg>
2025-11-17 11:22:50 +0300

    from collections import namedtuple

--- test.py
+++ test.py
@@ -1,19 +1,28 @@
-from collections import Counter
+# from collections import Counter
 
-a = "aaaaaaabbbcccc"
-my_counter = Counter(a)
-print(my_counter)
-e = my_counter.elements()
-while (True):
-	try:
-		print(next(e))
-	except StopIteration:
-		break
+# a = "aaaaaaabbbcccc"
+# my_counter = Counter(a)
+# print(my_counter)
+# e = my_counter.elements()
+# while (True):
+# 	try:
+# 		print(next(e))
+# 	except StopIteration:
+# 		break
+# ################################################
+# from collections import Counter
+
+# a = "aaaaaaabbbcccc"
+# my_counter = Counter(a)
+
+# for element in my_counter.elements():
+#     print(element)
+
 ################################################
-from collections import Counter
+def p():
+	print("Hey")
 
-a = "aaaaaaabbbcccc"
-my_counter = Counter(a)
-
-for element in my_counter.elements():
-    print(element)
+from collections import namedtuple
+Point = namedtuple('Point', 'x,y,z')
+pt = Point(1, p, 5)
+print(pt.y())
